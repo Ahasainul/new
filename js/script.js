3cbce64c2c9f7148408d6720abc58cda995eaa0d
Ahasainul <adnan31615asif@gmail.com>
2023-12-05 16:38:29 +0600

    first commits

---
+++ js/script.js
@@ -0,0 +1,75 @@
+
+//  start 
+let marks = 100
+
+if (marks >= 80 && marks <= 100) {
+  console.log('A+')
+}
+else if (marks >= 50 && marks <= 79) {
+  console.log('A-')
+}
+else if (marks >= 33 && marks <= 49) {
+  console.log('B+')
+
+}
+
+else if (marks < -0 && marks >= -1000) {
+  console.log('chalaki kom kor')
+}
+else if (marks >= 101 && marks <= 1000) {
+  console.log('paknami kom kor bata')
+}
+else {
+  console.log('You Are Dai')
+}
+// end 
+// start 
+let age = 22
+let home = 'chandpur'
+let study = 'educatate'
+
+if (age >= 22) {
+  if (home == 'chandpur') {
+    if (study == 'educatate') {
+      console.log('Yehaa sadi mibarak')
+
+    }
+    else {
+      console.log(' Raji na Ami')
+    }
+
+
+  }
+  else {
+    console.log(' Raji na Ami')
+  }
+
+}
+else {
+  console.log(' Raji na Ami')
+}
+// end 
+let team = ['Al-amin', 'asif', 'furkan', 'estak', 'rafi']
+console.log(team[0])
+
+// end 
+let man ={
+  age:18,
+  year:'dhaka',
+  jala:'chandpur',
+
+}
+console.log(man.jala) 
+// object arre end 
+// destactar stary 
+
+
+let {asif,sthan,alaka} ={
+  asif:18,
+  sthan:'dhaka',
+  alaka:'chandpur',
+
+}
+
+console.log(asif)
+// destactar end 
